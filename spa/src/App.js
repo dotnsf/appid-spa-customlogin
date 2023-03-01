976b2deb4f237c669dcb61ecf7c9c73d7c2e7edd
K.Kimura <dotnsf@gmail.com>
2023-03-01 11:29:00 +0900

    codes organized.

--- spa/src/App.js
+++ spa/src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import Main from './Main';
 
 function App() {
   return (
     <>
-    {/*
-    <React.Fragment>
-      <Routes>
-        <Route path="/*" element={<Main />} />
-      </Routes>
-    </React.Fragment>
-    */}
     <BrowserRouter>
       <Main />
     </BrowserRouter>
